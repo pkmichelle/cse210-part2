63cd1901c97f267632525117c4cfbe287c560983
pkmichelle <kli22008@byui.edu>
2025-01-09 19:45:37 -0700

    Completed c# prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What's your grade percentage? ");
+
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+        int lastDigit = grade % 10;
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade >= 50)
+        {
+            letter = "F";
+        }
+
+
+        if (letter!= "F")
+            if (lastDigit >= 7 && letter != "A")
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+        {
+            sign = "";
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine($"Your grade percentage is: {grade}%");
+        Console.WriteLine($"Your grade letter is: {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Congrats! You passed :)");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("I'm sorry, you didn't pass, but you'll get 'em next time!");
+        }
     }
 }
