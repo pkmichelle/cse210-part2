3de27658ae19ac3a68115bc5bb5aee1e2d22e802
pkmichelle <kli22008@byui.edu>
2025-01-14 14:33:14 -0700

    finished prep 3 + stretch 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+        int count = 0;
+
+        while (number != 0)
+        {
+            Console.WriteLine("What is your guess?");
+            string userGuess = Console.ReadLine();
+            int guess = int.Parse(userGuess);
+            count ++;
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+                Console.WriteLine("");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                Console.WriteLine("");
+            }
+            else 
+            {   
+                Console.WriteLine("That's right!");
+                Console.WriteLine($"Guesses: {count}");
+                Console.WriteLine("");
+                break;
+            }
+
+        }
+
     }
 }
